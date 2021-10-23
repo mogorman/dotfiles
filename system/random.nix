a5809e388a2f916eec6eee6c1741b46c32087050
matt o'gorman <mog@rldn.net>
2021-10-23 01:44:02 -0400

    allow tor unlock

--- system/random.nix
+++ system/random.nix
@@ -99,5 +99,38 @@
       "/home/mog/code/dotfiles/secrets/keys/random_initrd__ssh_host_ed25519_key"
     ];
   };
+
+boot.initrd.secrets = {
+  "/etc/tor/onion/bootup" = /home/mog/code/dotfiles/secrets/boot_onion; # maybe find a better spot to store this.
+};
+
+# copy tor to you initrd
+boot.initrd.extraUtilsCommands = ''
+  copy_bin_and_libs ${pkgs.tor}/bin/tor
+'';
+
+# start tor during boot process
+boot.initrd.network.postCommands = let
+  torRc = (pkgs.writeText "tor.rc" ''
+    DataDirectory /etc/tor
+    SOCKSPort 127.0.0.1:9050 IsolateDestAddr
+    SOCKSPort 127.0.0.1:9063
+    HiddenServiceDir /etc/tor/onion/bootup
+    HiddenServicePort 22 127.0.0.1:22
+  '');
+in ''
+  echo "tor: preparing onion folder"
+  sleep 3
+  # have to do this otherwise tor does not want to start
+  chmod -R 700 /etc/tor
+
+  echo "make sure localhost is up"
+  ip a a 127.0.0.1/8 dev lo
+  ip link set lo up
+
+  echo "tor: starting tor"
+  tor -f ${torRc} --verify-config
+  tor -f ${torRc} &
+'';
 }
 

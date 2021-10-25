2c55fad069b6358e4d5787a07e1f06dd112b62cd
matt o'gorman <mog@rldn.net>
2021-10-25 15:58:29 -0400

    mosquitto working locally with frigate

--- services/frigate.nix
+++ services/frigate.nix
@@ -1,8 +1,8 @@
 { config, lib, pkgs, ... }: {
+  imports = [ ../secrets/frigate.nix ];
   virtualisation.oci-containers.containers.frigate = {
     image = "blakeblackshear/frigate:stable-amd64";
     ports = [ "5000:5000" "1935:1935" ];
-    environmentFiles = [ ../secrets/frigate.env ];
     volumes = [
       "/state/frigate/media:/media/frigate"
       "${./frigate.yml}:/config/config.yml:ro"
@@ -10,6 +10,7 @@
     ];
     extraOptions = [
       "--shm-size=64m"
+      "--network=host"
       "--device=/dev/apex_0:/dev/apex_0"
       "--device=/dev/dri/renderD128"
       "--mount=type=tmpfs,target=/tmp/cache,tmpfs-size=1000000000"
--- services/mosquitto.nix
+++ services/mosquitto.nix
@@ -2,6 +2,6 @@
   imports = [ ../secrets/mosquitto_acl.nix ];
 
   services.mosquitto.enable = true;
-  services.mosquitto.allowAnonymous = true;
+  services.mosquitto.checkPasswords = true;
   systemd.services.mosquitto = { wants = [ "docker-frigate.service" ]; };
 }

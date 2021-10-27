c0d4273808c31a00a971820630e8ade629b1d310
matt o'gorman <mog@rldn.net>
2021-10-27 15:40:16 -0400

    doesnt work

--- services/homeassistant.nix
+++ services/homeassistant.nix
@@ -3,9 +3,6 @@
 services.home-assistant = {
   enable = true;
     package = (pkgs.unstable.home-assistant.override {
-      # pytestCheckPhase uses too much RAM and odyssey struggles with it
-      doCheck = false;
-      doInstallCheck = false;
       extraPackages = py: with py; [ psycopg2 aiohttp-cors ];
     });
     config.recorder.db_url = "postgresql://@/hass";

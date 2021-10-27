4ca6ea6d78623b461267711e0a210695654a3b5c
matt o'gorman <mog@rldn.net>
2021-10-27 18:26:45 -0400

    updated

--- services/homeassistant.nix
+++ services/homeassistant.nix
@@ -1,10 +1,20 @@
-{ config, lib, pkgs, ... }:
-{
-services.home-assistant = {
-  enable = true;
+{ config, lib, pkgs, ... }: {
+  services.home-assistant = {
+    enable = true;
     package = (pkgs.unstable.home-assistant.override {
-      extraPackages = py: with py; [ psycopg2 aiohttp-cors ];
+      extraPackages = py: with py; [ psycopg2 aiohttp-cors netdisco zeroconf ];
     });
-    config.recorder.db_url = "postgresql://@/hass";
+
+    config = {
+      homeassistant = {
+        name = "Home";
+      };
+      frontend = { themes = "!include_dir_merge_named themes"; };
+      http = { };
+
+      recorder.db_url = "postgresql://@/hass";
+      feedreader.urls = [ "https://nixos.org/blogs.xml" ];
+    };
+
   };
 }

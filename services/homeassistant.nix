22565ba0809a18a74a37244d475e42428b032e45
matt o'gorman <mog@rldn.net>
2021-10-28 14:47:51 -0400

    main parts of home-assistant i need to work working

--- services/homeassistant.nix
+++ services/homeassistant.nix
@@ -1,14 +1,83 @@
-{ config, lib,inputs, pkgs, ... }: {
+{ config, lib, inputs, pkgs, ... }: {
   services.home-assistant = {
     enable = true;
+    configWritable = true;
+    lovelaceConfigWritable = true;
     package = (pkgs.unstable.home-assistant.override {
-      extraPackages = py: with py; [ psycopg2 aiohttp-cors netdisco zeroconf pymetno pyipp brother ];
+      extraPackages = py:
+        with py; [
+          psycopg2
+          aiohttp-cors
+          netdisco
+          zeroconf
+          pymetno
+          pyipp
+          brother
+        ];
     });
+    lovelaceConfig = {
+      resources = [
+        {
+          url = "/local/frigate-hass-card.js";
+          type = "module";
+        }
+        {
+          url = "/local/simple-thermostat.js";
+          type = "module";
+        }
+      ];
+
+      title = "Home";
+      views = [{
+        title = "Example";
+        cards = [
+          {
+            type = "markdown";
+            title = "Lovelace";
+            content = "Welcome to your **Lovelace UI**.";
+          }
+          {
+            type = "entities";
+            title = "Person";
+            entities = [ "person.mog" ];
+          }
+          {
+            type = "entities";
+            title = "Sensor";
+            entities = [
+              "sensor.back_camera_fps"
+              "sensor.back_detection_fps"
+              "sensor.back_dog"
+              "sensor.back_mouse"
+              "sensor.back_person"
+              "sensor.back_process_fps"
+              "sensor.back_skipped_fps"
+              "sensor.coral_inference_speed"
+              "sensor.detection_fps"
+            ];
+          }
+          {
+            type = "custom:frigate-card";
+            dimensions = { };
+            controls = { nextprev = "chevrons"; };
+            live_provider = "webrtc";
+            camera_entity = "camera.back";
+            webrtc = { url = "rtsp://192.168.3.246:8554/unicast"; };
+            view_default = "snapshot";
+            menu_buttons = {
+              frigate_ui = true;
+              fullscreen = true;
+              frigate = false;
+            };
+            menu_mode = "hover-top";
+          }
+
+        ];
+      }];
+    };
 
     config = {
-      homeassistant = {
-        name = "Home";
-      };
+      homeassistant = { name = "Home"; };
       frontend = { themes = "!include_dir_merge_named themes"; };
       http = { };
       default_config = { };
@@ -19,14 +88,18 @@
       zeroconf = { };
       webrtc = { };
       frigate = { };
-      mqtt = { };
+      mqtt = {
+        broker = "localhost";
+        username = "home";
+        password = "homesliceyo";
+        discovery = true;
+        discovery_prefix = "homeassistant";
+
+      };
       recorder.db_url = "postgresql://@/hass";
       feedreader.urls = [ "https://nixos.org/blogs.xml" ];
-      zha = {
-        database_path = "/state/hass/zigbee.db";
-      };
+      zha = { database_path = "/state/hass/zigbee.db"; };
     };
-
   };
 
   systemd.tmpfiles.rules = [
@@ -36,9 +109,12 @@
     "L /var/lib/hass/custom_components/webrtc - - - - ${inputs.webrtc-card}/custom_components/webrtc"
 
     # #front end
-    # "C /var/lib/hass/www/community/frigate-hass-card/frigate-hass-card.js - - - - ${sources.frigate-hass-card}/frigate-hass-card.js"
-    # "C /var/lib/hass/www/community/simple-thermostat/simple-thermostat.js - - - - ${sources.simple-thermostat-card}/simple-thermostat.js"
-    # "Z /var/lib/hass/www/ 770 hass hass - -"
-    # "Z /var/lib/hass/www/community 770 hass hass - -"
+    "d /var/lib/hass/www 0755 hass hass"
+    "C /var/lib/hass/www/frigate-hass-card.js 0755 hass hass - ${
+      ../hass/frontend/frigate-hass-card.js
+    }"
+    "C /var/lib/hass/www/simple-thermostat.js 0755 hass hass - ${
+      ../hass/frontend/simple-thermostat.js
+    }"
   ];
 }
--- update.sh
+++ update.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 nix flake update
+wget -O ./hass/frontend/frigate-hass-card.js https://github.com/dermotduffy/frigate-hass-card/releases/latest/download/frigate-hass-card.js
+wget -O ./hass/frontend/simple-thermostat.js https://github.com/nervetattoo/simple-thermostat/releases/latest/download/simple-thermostat.js

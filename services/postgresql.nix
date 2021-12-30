5ee346726a7d5da2d10616ef56e4f385da68a63a
matt o'gorman <mog@rldn.net>
2021-12-30 00:58:58 -0500

    updated postgres

--- services/postgresql.nix
+++ services/postgresql.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }: {
   services.postgresql = {
     enable = true;
+    package = pkgs.postgresql_13;
     ensureDatabases = [ "hass" ];
     dataDir = "/state/postgresql/${config.services.postgresql.package.psqlSchema}";
     ensureUsers = [{

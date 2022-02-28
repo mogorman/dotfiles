026a62741d24c453cb272836ede486276d8e6525
mog <mog@rldn.net>
2022-02-28 13:51:32 -0500

    dumb update

--- system/common.nix
+++ system/common.nix
@@ -2,7 +2,7 @@
 
 {
   nixpkgs.config.allowUnfree = true;
-  system.stateVersion = "21.05";
+  system.stateVersion = "21.11";
   fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
   time.timeZone = "US/Eastern";
 }
--- system/madrox.nix
+++ system/madrox.nix
@@ -7,6 +7,165 @@
     ../users/mog.nix
   ];
 
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/1e6306dd-a288-4077-901a-d92941c6b3ae";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/A306-D1CE";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/6221890a-688e-4a27-beb1-a5b5fd19dd4a"; }
+    ];
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  virtualisation.hypervGuest.enable = true;
+
+#  imports =
+#    [ # Include the results of the hardware scan.
+##      <nixos-hardware/microsoft/surface/firmware/surface-go/ath10k>
+#      ./hardware-configuration.nix
+#    ];
+
+  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  nixpkgs.config.allowUnfree = true;
+#  hardware.microsoft-surface.firmware.surface-go-ath10k.replace = true;
+
+ hardware.enableRedistributableFirmware = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;  
+  hardware.sensor.iio.enable = true;
+boot.extraModprobeConfig = ''
+    options i915 enable_fbc=1 enable_rc6=1 modeset=1
+    options snd_hda_intel power_save=1
+    options snd_ac97_codec power_save=1
+    options iwlwifi power_save=Y
+  '';
+
+    # options iwldvm force_cam=N
+  fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
+  boot.kernelParams = [ "intel_pstate=no_hwp" "mem_sleep_default=deep" ];
+
+  boot.initrd.luks.devices.luksroot = { 
+     device = "/dev/disk/by-uuid/4d4f1f02-c41e-410b-afe4-22556ec26467";
+     preLVM = true; 
+     allowDiscards = true; 
+  };
+#
+#  boot.initrd.luks.devices = [
+#    {
+#      name = "root";
+#      device = "/dev/disk/by-uuid/4d4f1f02-c41e-410b-afe4-22556ec26467";
+#      preLVM = true;
+#      allowDiscards = true;
+#    }
+#  ];
+#
+  networking.hostName = "madrox"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "US/Eastern";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.networkmanager.enable = true;
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.mog = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+   };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+   environment.systemPackages = with pkgs; [
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     wget
+     firefox
+     gnome.gnome-tweaks
+     jellyfin-media-player
+     tilix
+     vlc
+     usbutils
+     git
+     git-crypt
+     gpa
+   ];
+ 
+  programs.steam.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+   programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+   };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+   services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+   networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+
 }

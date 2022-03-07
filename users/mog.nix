5db033cb0a90028ba25e47a4ace197883e4646a5
Matthew O'Gorman <mog@rldn.net>
2022-03-07 02:54:48 -0500

    add user to network manager group

--- users/mog.nix
+++ users/mog.nix
@@ -3,7 +3,7 @@
   users.users.mog = {
     isNormalUser = true;
     openssh.authorizedKeys.keys = config.mog_keys;
-    extraGroups = [ "wheel" "docker" "render" "media" "video" ];
+    extraGroups = [ "wheel" "docker" "render" "media" "video" "networkmanager" ];
   };
 
 }

c73238657af1de958c2d733047d224d3d44cef81
matt o'gorman <mog@rldn.net>
2022-03-12 00:07:03 -0500

    adding wireguard

--- packages/sdm.nix
+++ packages/sdm.nix
@@ -5,8 +5,9 @@ in stdenv.mkDerivation {
   name = "sdm";
 
   src = builtins.fetchurl {
-    url = "https://app.strongdm.com/releases/cli/linux";
-    sha256 = "sha256:0jb31xxkr5rppblq0gvxw59vaa0859wdvcj4a1h822frxf5nnzjh";
+    #url = "https://app.strongdm.com/releases/cli/linux";
+    url = "https://sdm-releases-production.s3.amazonaws.com/builds/sdm-cli/32.61.0/linux/amd64/FB5A678E3D2FE5BDA94124801D068C70EA1950A3/sdmcli_32.61.0_linux_amd64.zip";
+     sha256 = "sha256:1mfgl9b8gm569k0y7s77ya3zi3l4gnwvk5xia80pzj0zg1wm8pz8";
   };
 
   # preUnpack = ''
--- system/madrox.nix
+++ system/madrox.nix
@@ -44,6 +44,42 @@
 
   networking.hostName = "madrox"; # Define your hostname.
 
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the client's end of the tunnel interface.
+      ips = [ "10.100.42.2/24" ];
+      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
+
+      # Path to the private key file.
+      #
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKeyFile = "${../secrets/madrox_private}";
+
+      peers = [
+        # For a client configuration, one peer entry for the server will suffice.
+
+        {
+          # Public key of the server (not a file path).
+          publicKey = "FEa4tK4tWa3xWCtzrLMBFTcpMVzu/S4xKm7VKxb4TDQ=";
+
+          # Forward all the traffic via VPN.
+          #allowedIPs = [ "0.0.0.0/0" ];
+          # Or forward only particular subnets
+          allowedIPs = [ "10.0.42.0/24" ];
+
+          # Set this to the server IP and port.
+          endpoint = "73.114.145.39:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+
+          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+
   boot.initrd.availableKernelModules =
     [ "xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ "dm-snapshot" "ecryptfs" ];

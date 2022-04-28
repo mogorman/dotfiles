22c5991baada7f76d40df8b717bfa9798fdbed45
matt o'gorman <mog@rldn.net>
2022-04-28 02:30:29 -0400

    forgot semicolon

--- services/homeassistant.nix
+++ services/homeassistant.nix
@@ -418,7 +418,7 @@
           platform = "mqtt_room";
           device_id = "iBeacon:426c7565-4368-6172-6d42-6561636f6e73-3838-4949";
           name =  "Tommy";
-          state_topic = "espresense/rooms"
+          state_topic = "espresense/rooms";
           timeout = 20;
           away_timeout = 40; # number of seconds after which the enitity will get status not_home
         }

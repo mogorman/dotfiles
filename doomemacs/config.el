6c45a55fbf94f85491854d4e92a7a37ced0f5175
Matthew O'Gorman <mog@rldn.net>
2023-06-16 14:35:17 -0400

    updates to doom emacs

--- doomemacs/config.el
+++ doomemacs/config.el
@@ -1,77 +1,30 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Matthew O'Gorman"
       user-mail-address "mog@rldn.net"
       git-committer-name "Matthew O'Gorman"
       git-committer-email "mog@rldn.net"
       )
-(server-start)
-;;(setq exec-path (append exec-path '("/home/mog/code/pepsico/elixir-ls/release")))
-(setq lsp-elixir-server-command '("elixir-ls"))
-;;(setq lsp-elixir-local-server-command  '("elixir-ls"))
 (menu-bar-mode 1)
 
 (global-set-key (kbd "C-c C-t") '+vterm/toggle)
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
- (setq doom-font (font-spec :family "Victor Mono" :size 12 :weight 'medium)
-       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(global-set-key (kbd "C-c C-n") 'multi-vterm)
+(global-set-key (kbd "C-c C-d") '+default/diagnostics)
+(global-set-key (kbd "C-x w") 'vc-msg-show)
+(global-set-key (kbd "C-c C-v") 'vterm-copy-mode)
 
+(setq doom-font (font-spec :family "Victor Mono" :size 12 :weight 'medium)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-molokai)
+(setq doom-theme 'doom-monokai-machine)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
-(setq company-lsp-cache-candidates 'auto)
-(setq lsp-elixir-dialyzer-enabled nil)
+(setq org-directory "~/org/")
 
-(defun lsp-elixir-set-configuration ()
-  (let ((config `(:elixirLS (:mixEnv "dev" :dialyzerEnabled :json-false))))
-    (lsp--set-configuration config)
-    ))
-(add-hook 'lsp-after-initialize-hook 'lsp-elixir-set-configuration)
+;;(setq lsp-elixir-server-command '("elixir-ls"))
+(setq lsp-elixir-server-command '("/home/mog/code/pepsico/lexical/_build/dev/rel/lexical/start_lexical.sh"))
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
 
 (setq lsp-file-watch-ignored-directories
       (quote
@@ -84,93 +37,14 @@
         "[/\\\\]node_modules$"
         )))
 
-;;; lang/elixir/config.el -*- lexical-binding: t; -*-
-;;;
-(add-hook 'elixir-mode-hook
-          (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
+(setq vterm-buffer-name-string "vt:%s")
 
-(use-package! lsp-mode
-  :hook (elixir-mode . lsp))
-(setq lsp-file-watch-threshold 2000)
-
-(after! projectile
-  (add-to-list 'projectile-project-root-files "mix.exs"))
-
-
-;;
-;;; Packages
-
-(use-package! elixir-mode
-  :defer t
-  :init
-  ;; Disable default smartparens config. There are too many pairs; we only want
-  ;; a subset of them (defined below).
-  (provide 'smartparens-elixir)
-  :config
-  (set-ligatures! 'elixir-mode
-    ;; Functional
-    :def "def"
-    :lambda "fn"
-    ;; :src_block "do"
-    ;; :src_block_end "end"
-    ;; Flow
-    :not "!"
-    :in "in" :not-in "not in"
-    :and "and" :or "or"
-    :for "for"
-    :return "return" :yield "use")
-
-  ;; ...and only complete the basics
-  (sp-with-modes 'elixir-mode
-    (sp-local-pair "do" "end"
-                   :when '(("RET" "<evil-ret>"))
-                   :unless '(sp-in-comment-p sp-in-string-p)
-                   :post-handlers '("||\n[i]"))
-    (sp-local-pair "do " " end" :unless '(sp-in-comment-p sp-in-string-p))
-    (sp-local-pair "fn " " end" :unless '(sp-in-comment-p sp-in-string-p)))
-
-  ;;(add-hook! lsp-mode (elixir-mode . lsp))
-  ;; (add-hook 'elixir-mode-local-vars-hook #'lsp!)
-  ;;   (after! lsp-mode
-  ;;     (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\_build\\'"))
-
-  (after! highlight-numbers
-    (puthash 'elixir-mode
-             "\\_<-?[[:digit:]]+\\(?:_[[:digit:]]\\{3\\}\\)*\\_>"
-             highlight-numbers-modelist)))
-
-
-(use-package! flycheck-credo
-  :when (featurep! :checkers syntax)
-  :after elixir-mode
-  :config (flycheck-credo-setup))
-
-(use-package! exunit
-  :hook (elixir-mode . exunit-mode)
-  :init
-  (map! :after elixir-mode
-        :localleader
-        :map elixir-mode-map
-        :prefix ("t" . "test")
-        "a" #'exunit-verify-all
-        "r" #'exunit-rerun
-        "v" #'exunit-verify
-        "T" #'exunit-toggle-file-and-test
-        "t" #'exunit-toggle-file-and-test-other-window
-        "s" #'exunit-verify-single))
-
-(after! doom-modeline
-  (setq doom-modeline-major-mode-icon t)
-  (setq doom-modeline-buffer-file-name-style 'truncate-except-project)
-  ;; Define your custom doom-modeline
-(doom-modeline-def-modeline 'my-simple-line
- '(bar buffer-info remote-host  parrot selection-info)
- '(misc-info minor-modes input-method process lsp checker vcs))
-
-;;Add to `doom-modeline-mode-hook` or other hooks
-(defun setup-custom-doom-modeline ()
-  (doom-modeline-set-modeline 'my-simple-line 'default))
-(add-hook 'doom-modeline-mode-hook 'setup-custom-doom-modeline)
-)
-;; #9370db purple
-;;(set-face-attribute 'mode-line nil :background "#9370db")
+(defun mog/vterm--set-title (title)
+  (message "called")
+  "Use TITLE to set the buffer name according to `vterm-buffer-name-string'."
+  (if (string-equal (buffer-name) "*doom:vterm-popup:main*")
+      (message "not renaming")
+    (when vterm-buffer-name-string
+      (rename-buffer (format vterm-buffer-name-string title) t))
+    ))
+(advice-add 'vterm--set-title :override #'mog/vterm--set-title)
--- doomemacs/custom.el
+++ doomemacs/custom.el
@@ -1,23 +1,92 @@
-:(custom-set-variables
+(custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
- '(column-number-mode t)
+ '(connection-local-criteria-alist
+   '(((:application tramp)
+      tramp-connection-local-default-system-profile tramp-connection-local-default-shell-profile)))
+ '(connection-local-profile-alist
+   '((tramp-connection-local-darwin-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state=abcde" "-o" "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (euid . number)
+       (user . string)
+       (egid . number)
+       (comm . 52)
+       (state . 5)
+       (ppid . number)
+       (pgrp . number)
+       (sess . number)
+       (ttname . string)
+       (tpgid . number)
+       (minflt . number)
+       (majflt . number)
+       (time . tramp-ps-time)
+       (pri . number)
+       (nice . number)
+       (vsize . number)
+       (rss . number)
+       (etime . tramp-ps-time)
+       (pcpu . number)
+       (pmem . number)
+       (args)))
+     (tramp-connection-local-busybox-ps-profile
+      (tramp-process-attributes-ps-args "-o" "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "stat=abcde" "-o" "ppid,pgid,tty,time,nice,etime,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (user . string)
+       (group . string)
+       (comm . 52)
+       (state . 5)
+       (ppid . number)
+       (pgrp . number)
+       (ttname . string)
+       (time . tramp-ps-time)
+       (nice . number)
+       (etime . tramp-ps-time)
+       (args)))
+     (tramp-connection-local-bsd-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (euid . number)
+       (user . string)
+       (egid . number)
+       (group . string)
+       (comm . 52)
+       (state . string)
+       (ppid . number)
+       (pgrp . number)
+       (sess . number)
+       (ttname . string)
+       (tpgid . number)
+       (minflt . number)
+       (majflt . number)
+       (time . tramp-ps-time)
+       (pri . number)
+       (nice . number)
+       (vsize . number)
+       (rss . number)
+       (etime . number)
+       (pcpu . number)
+       (pmem . number)
+       (args)))
+     (tramp-connection-local-default-shell-profile
+      (shell-file-name . "/bin/sh")
+      (shell-command-switch . "-c"))
+     (tramp-connection-local-default-system-profile
+      (path-separator . ":")
+      (null-device . "/dev/null"))))
  '(custom-safe-themes
-   '("4b6b6b0a44a40f3586f0f641c25340718c7c626cbf163a78b5a399fbe0226659" "a82ab9f1308b4e10684815b08c9cac6b07d5ccb12491f44a942d845b406b0296" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "4133d2d6553fe5af2ce3f24b7267af475b5e839069ba0e5c80416aa28913e89a" "1278c5f263cdb064b5c86ab7aa0a76552082cf0189acf6df17269219ba496053" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "6f4421bf31387397f6710b6f6381c448d1a71944d9e9da4e0057b3fe5d6f2fad" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" default))
- '(doom-molokai-brighter-modeline t)
- '(size-indication-mode t)
+   '("3fe1ebb870cc8a28e69763dde7b08c0f6b7e71cc310ffc3394622e5df6e4f0da" "b54376ec363568656d54578d28b95382854f62b74c32077821fdfd604268616a" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "8d8207a39e18e2cc95ebddf62f841442d36fcba01a2a9451773d4ed30b632443" "251ed7ecd97af314cd77b07359a09da12dcd97be35e3ab761d4a92d8d8cf9a71" "5586a5db9dadef93b6b6e72720205a4fa92fd60e4ccfd3a5fa389782eab2371b" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "991ca4dbb23cab4f45c1463c187ac80de9e6a718edc8640003892a2523cb6259" "2f8eadc12bf60b581674a41ddc319a40ed373dd4a7c577933acaff15d2bf7cc6" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "ddffe74bc4bf2c332c2c3f67f1b8141ee1de8fd6b7be103ade50abb97fe70f0c" "b99e334a4019a2caa71e1d6445fc346c6f074a05fcbb989800ecbe54474ae1b0" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "2dd4951e967990396142ec54d376cced3f135810b2b69920e77103e0bcedfba9" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "4b6b6b0a44a40f3586f0f641c25340718c7c626cbf163a78b5a399fbe0226659" "a82ab9f1308b4e10684815b08c9cac6b07d5ccb12491f44a942d845b406b0296" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "4133d2d6553fe5af2ce3f24b7267af475b5e839069ba0e5c80416aa28913e89a" "1278c5f263cdb064b5c86ab7aa0a76552082cf0189acf6df17269219ba496053" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "6f4421bf31387397f6710b6f6381c448d1a71944d9e9da4e0057b3fe5d6f2fad" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" default))
+ '(line-number-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:foreground "LightSteelBlue3" :slant italic))))
- '(font-lock-commment-face ((t (:slant 'italic))))
- '(font-lock-string-face ((t (:foreground "#e2c770" :slant italic))))
- '(hl-line ((t nil)))
- '(ivy-current-match ((t (:extend t :background "medium purple" :foreground "white")))))
-(put 'customize-themes 'disabled nil)
+ '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
